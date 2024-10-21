7646cd35cbd40ca9aa29060328db831358b6a008
nikashs26 <nikash926@gmail.com>
2024-10-21 11:32:05 -0700

    wrote javascript

---
+++ week5/index.js
@@ -0,0 +1,22 @@
+//console.log("hello world")
+//alert('Upgrade your VPN! CLICK BELOW')
+window.document.write('<p>Welcome to CSEN 60!</p>');
+
+//window.document.append('<p>This should come after the h1</p>')
+
+/*
+  Function to calculate the sum of two numbers.
+  Inputs: a, b - numbers to be added.
+  Output: returns the sum of a and b.
+*/
+//TODO fix this function
+
+/**
+ * @description
+ * @param {*}a
+ * @param {*}b
+ * @returns
+ */ 
+function add(a, b) {
+    return a + b;
+}
